1f8d3cd094a4fe312ef3dfbb75eb323d912145bd
Matt Walker <walkermatt@longwayaround.org.uk>
2011-02-13 15:50:59 +0000

    first commit

---
+++ basecamp_search.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name           Basecamp Search
+// @namespace      uk.org.longwayaround
+// @description    Adds a search field to basecamp to search the current project
+// @include        https://astun.basecamphq.com/projects/*
+// ==/UserScript==
+
+var prjNum = window.location.pathname.split('/')[2];
+var header = document.getElementById('Header');
+var input = document.createElement('input');
+input.id = 'search';
+input.type = 'text';
+input.style.position = 'absolute';
+input.style.right = '23px';
+input.style.top = '24px';
+input.style.width = '207px';
+header.appendChild(input);
+input.addEventListener('keyup',function(e) {
+    if (e.keyCode == 13) {
+	window.location.href='/projects/' + prjNum + '/search?scope=all&terms=' + input.value;
+    }
+},false);
